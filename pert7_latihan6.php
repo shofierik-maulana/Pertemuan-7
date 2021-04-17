15665c425d7f30eb88b5ed1c75ceabf67f78a30e
Shofierikmaulana <62019573+shofierik-maulana@users.noreply.github.com>
2021-04-17 15:49:10 +0700

    Add files via upload

---
+++ pert7_latihan6.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penggunaan List 2</title>
+</head>
+<body>
+<?php 
+$program = array('Bobo','Doraemon','Spiderman'); list($Majalah, , $Film) = $program; echo "Jenis Buku & Hiburan :"; echo "<br />"; echo "Cerpen : $Majalah"; echo "<br />"; echo "Bioskop : $Film";
+ ?>
+</body>
+</html>
---
+++ pert7_latihan8.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penggunaan Join</title>
+</head>
+<body>
+<?php  
+$var = array('18', '11', '2010'); $tanggal = join("/", $var); echo "$tanggal";
+?>
+</body>
+</html>
